befc8666ac0de4f68cf0d03f80fc04681616c148
Pascal Nitsche <saikootau@gmail.com>
2021-11-09 18:35:35 +0100

    chore: add M1 MacBook playbook
    Signed-off-by: Pascal Nitsche <saikootau@gmail.com>

--- init_mac.sh
+++ init_mac.sh
@@ -3,6 +3,9 @@
 echo "-> Installing homebrew"
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
+echo "-> Ensuring brew environment"
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
 echo "-> Installing ansible"
 brew update
 brew install ansible
